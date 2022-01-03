81011ab211edde68b27ecde9d3bc29f283ae13f6
gokhanForesight <89521716+gokhanForesight@users.noreply.github.com>
2022-01-03 12:15:31 +0300

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,4 +6,4 @@ set -x
 # Check README.md is up to date
 python ./scripts/docs.py verify-readme
 export PYTHONPATH=./docs_src
-pytest --workers auto --cov=fastapi --cov=tests --cov=docs_src --cov-report=term-missing:skip-covered --cov-report=xml tests --junitxml=result.xml ${@}
+pytest --workers auto --cov=fastapi --cov=tests --cov=docs_src --cov-report=term-missing:skip-covered --cov-report=xml tests --junitxml=reports/result.xml ${@}
